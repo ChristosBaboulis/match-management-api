6fdab6f10bf4b6f23e38cc6e8c620c880a6a1ef6
ChristosBaboulis <chrisb2603@gmail.com>
2025-07-23 22:11:36 +0300

    Initial commit

---
+++ src/main/java/com/example/matchmanagementapi/MatchManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.matchmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matchmanagementapi/MatchManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matchmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
