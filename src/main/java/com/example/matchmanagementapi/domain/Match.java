ddcd4627ba2767083b63c48c721e63641bb1b267
ChristosBaboulis <chrisb2603@gmail.com>
2025-07-24 20:53:13 +0300

    Code Revewing on Match class

--- src/main/java/com/example/matchmanagementapi/domain/Match.java
+++ src/main/java/com/example/matchmanagementapi/domain/Match.java
@@ -6,44 +6,42 @@ import lombok.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-
+@Entity
+@Table(name = "matches")
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @RequiredArgsConstructor
 @ToString
 @EqualsAndHashCode(exclude = {"id"})
-@Entity
-@Table(name = "matches")
 public class Match {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     private Long id;
 
     @NonNull
-    @Column(name="description", nullable=false)
+    @Column(name = "description", nullable=false)
     private String description;
 
     @NonNull
-    @Column(name="match_date", nullable = false)
+    @Column(name = "match_date", nullable = false)
     private LocalDate matchDate;
 
     @NonNull
-    @Column(name="match_time", nullable = false)
+    @Column(name = "match_time", nullable = false)
     private LocalTime matchTime;
 
     @NonNull
-    @Column(name="team_a", nullable = false)
+    @Column(name = "team_a", nullable = false)
     private String teamA;
 
     @NonNull
-    @Column(name="team_b", nullable = false)
+    @Column(name = "team_b", nullable = false)
     private String teamB;
 
     @NonNull
-    @Column(name="sport", nullable = false)
+    @Column(name = "sport", nullable = false)
     @Enumerated(EnumType.ORDINAL)
     private Sport sport;
 }
